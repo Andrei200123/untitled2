d63457ba92b7283df57dc465dcd82af2296d8c26
Andrei200123 <andrei-vorontsoff2013@yandex.ru>
2022-02-15 17:28:51 +0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+      home: Scaffold(
+    appBar: AppBar(
+        title:
+            const Text('Лучшая надпись',
+                style: TextStyle(color: Colors.white))),
+    body: Padding(padding: ,
+        ),
+  )));
+}
